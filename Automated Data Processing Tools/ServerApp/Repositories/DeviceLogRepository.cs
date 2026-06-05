e7f37743e027382080d273c6455b1ccc92be1804
Gupta Dinesh <dinesh.k.gupta@youtheng.co.jp>
2026-06-05 18:55:56 +0900

    Server code edited

--- Automated Data Processing Tools/ServerApp/Repositories/DeviceLogRepository.cs
+++ Automated Data Processing Tools/ServerApp/Repositories/DeviceLogRepository.cs
@@ -1,116 +1,71 @@
 ﻿using Dapper;
 using Microsoft.Data.SqlClient;
-using Serilog;
 using ServerApp.Models;
-using ServerApp.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Text;
-using System.Text.Json;
 
 namespace ServerApp.Repositories
 {
     class DeviceLogRepository
     {
-        //DB接続
-        //SQL　一覧表示　追加　更新　削除
+        private readonly string _connectionString = @"Server=(localdb)\MSSQLLocalDB;
+                                                         Database=TrainingDB;
+                                                         Trusted_Connection=True;
+                                                         TrustServerCertificate=True;";
+        public List<DeviceLog> GetAll()
 
-        private readonly string _connectionString;
-
-        public DeviceLogRepository(string connectionString)
         {
-            _connectionString = connectionString;
+            using var connection = new SqlConnection(_connectionString);
+            return connection.Query<DeviceLog>("SELECT * FROM DeviceLogs").ToList();
         }
 
-
-
-
-
-        public async Task<IEnumerable<DeviceLog>> GetAll()
+        public void Add(DeviceLog log)
         {
-            try
-            {
-                Log.Information("GetAll start");
-                using var con = new SqlConnection(_connectionString);
+            using var connection = new SqlConnection(_connectionString);
+            string sql = """
+       　　　　　　　　　　 INSERT INTO DeviceLogs
+       　　　　　　　　　　 (
+       　　　　　　　　　　     DeviceName,
+       　　　　　　　　　　     Amount,
+       　　　　　　　　　　     ErrorCode,
+       　　　　　　　　　　     Timestamp
+       　　　　　　　　　　 )
+       　　　　　　　　　　 VALUES
+       　　　　　　　　　　 (
+       　　　　　　　　　　     @DeviceName,
+       　　　　　　　　　　     @Amount,
+       　　　　　　　　　　     @ErrorCode,
+       　　　　　　　　　　     GETDATE()
+       　　　　　　　　　　 )
+       　　　　　　　　　　 """;
 
-                return await con.QueryAsync<DeviceLog>("SELECT * FROM DeviceLogs");
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex,"GetAll failed");
-                return Enumerable.Empty<DeviceLog>();
-            }
+            connection.Execute(sql, log);
         }
 
-
-
-        public async Task<int> Add(DeviceLog log)
+        public void Update(DeviceLog log)
         {
-            if (log == null)
-                throw new ArgumentNullException(nameof(log));
+            using var connection = new SqlConnection(_connectionString);
+            string sql = """
+       　　　　　　　　　　 UPDATE DeviceLogs
+       　　　　　　　　　　 SET
+       　　　　　　　　　　     DeviceName=@DeviceName,
+       　　　　　　　　　　     Amount=@Amount,
+       　　　　　　　　　　     ErrorCode=@ErrorCode
+       　　　　　　　　　　 WHERE
+       　　　　　　　　　　     Id=@Id
+       　　　　　　　　　　 """;
 
-            Log.Information("Add start: {@Log}", log);
-
-
-            using var con = new SqlConnection(_connectionString);
-
-            string sql = @"
-        INSERT INTO DeviceLogs
-        (
-            DeviceName,
-            Amount,
-            ErrorCode,
-            Timestamp
-        )
-        VALUES
-        (
-            @DeviceName,
-            @Amount,
-            @ErrorCode,
-            @Timestamp
-        )";
-
-            return await con.ExecuteAsync(sql, log);
+            connection.Execute(sql, log);
         }
 
-
-
-
-        public  async Task<int> Update(DeviceLog log)
+        public void Delete(int id)
         {
-            if (log == null)
-                throw new ArgumentNullException(nameof(log));
+            using var connection = new SqlConnection(_connectionString);
+            connection.Execute("DELETE FROM DeviceLogs WHERE Id=@Id",
+                               new { Id = id });
 
-            Log.Information("Update start: {@Log}", log);
-
-            using var con = new SqlConnection(_connectionString);
-
-            string sql = @"
-            UPDATE DeviceLogs
-            SET
-                DeviceName=@DeviceName,
-                Amount=@Amount,
-                ErrorCode=@ErrorCode
-            WHERE Id=@Id";
-
-            return await con.ExecuteAsync(sql, log);
-        }
-
-
-
-
-
-        public async Task<int> Delete(int id)
-        {
-            Log.Information("Delete id: {Id}", id);
-
-            using var con = new SqlConnection(_connectionString);
-
-            return await con.ExecuteAsync(
-                "DELETE FROM DeviceLogs WHERE Id=@Id",
-                new { Id = id });
         }
     }
-
-
 }
