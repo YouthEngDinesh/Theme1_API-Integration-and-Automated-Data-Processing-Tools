18362961db4f404272791d355168abbe2b2630dc
Gupta Dinesh <dinesh.k.gupta@youtheng.co.jp>
2026-06-05 13:50:07 +0900

    Service class corrected

--- Automated Data Processing Tools/ServerApp/obj/Debug/net10.0/ServerApp.AssemblyInfo.cs
+++ Automated Data Processing Tools/ServerApp/obj/Debug/net10.0/ServerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6aca8e967d1181ebccd4483f04c9bb2e3f6c28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e23ab616bcaba8ac7bd4f8a6714aa336b3c37e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
